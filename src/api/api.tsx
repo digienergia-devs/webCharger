a58c177ba05aa7b6e08e06f982dd6ac22134eba0
dimansha <dimansha.wijebandara@gmail.com>
2024-03-27 22:47:35 +0200

    Try to add new middle layer APIs

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,18 +1,24 @@
 import axios from "axios";
 
-const API_BASE_URL = "/";
+const API_BASE_URL = "http://185.96.163.154:8080";
 
 const api = axios.create({
     baseURL: API_BASE_URL,
 });
 
-export async function startChargerConnection(chargerID: any) {
+export async function startChargerConnection(requestBody: any) {
     const endpoint = "/api/start-charger-connection";
-    const payload = chargerID;
+
+    /* { this should be in the request body
+        "chargerID": "f9f16925-28b8-4ba5-99f2-d8080f0860f3-2",
+        "userID": "4dc7736d-b6e9-4006-a3a6-51653cc6cd53"
+    } */
 
     try {
-        const response = await api.post(endpoint, payload);
+        console.log("start charger connection request ---")
+        const response = await api.post(endpoint, requestBody);
         if (response.status == 200) {
+            console.log("start charger connection response ---", response.data);
             return (response.data)
         } else {
             return ('error')
---
+++ src/setupProxy.ts
@@ -0,0 +1,26 @@
+// src/setupProxy.ts
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app: any) {
+    // Proxy the first API route
+    app.use(
+        '/api/start-charger-connection',  
+        createProxyMiddleware({
+            target: 'http://185.96.163.154:8080',  
+            changeOrigin: true,
+            secure: false  
+        })
+    );
+
+    // Proxy the second API route
+    /* app.use(
+        '/api/route2',  
+        createProxyMiddleware({
+            target: 'http://backend-api-url.com',  
+            changeOrigin: true,
+            secure: false  
+        })
+    ); */
+
+    // Add more API routes as needed
+};
