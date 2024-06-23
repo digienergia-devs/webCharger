f792b9a162ba02098dcc069d692399b64ec2674c
dimansha <dimansha.wijebandara@gmail.com>
2024-06-23 10:14:04 +0300

    remove charging session header

--- src/pages/chargingSessionScreen/index.tsx
+++ src/pages/chargingSessionScreen/index.tsx
@@ -187,14 +187,14 @@ export default function ChargingSessionScreen(props: any) {
             <div className="flex justify-center items-center h-1/6">
                 <img src={require('../../assets/icons/Final3.png')} alt="" />
             </div>
-            <div className='flex flex-col justify-center items-center h-1/6 w-full'>
+            {/* <div className='flex flex-col justify-center items-center h-1/6 w-full'>
                 <div className='flex justify-center items-center h-1/2'>
 
                 </div>
                 <div className='flex h-1/2 justify-center items-center text-center rounded-tl-30 rounded-tr-30 bg-iparkOrange400 w-5/6 shadow-md text-white font-bold text-xs md:text-md xl:text-xl'>
                     <p className="m-0">Charging Session</p>
                 </div>
-            </div>
+            </div> */}
             <div className='flex flex-col justify-center rounded-tl-30 rounded-tr-30 items-center h-4/6 w-screen bg-white'>
                 <div className="flex p-5 m-5 justify-center flex-col items-center rounded-tl-30 rounded-tr-30 rounded-bl-30 rounded-br-30 bg-gray-100 w-5/6 shadow-md text-gray-400 text-xs md:text-md xl:text-xl" style={{ textShadow: '1px 1px 2px rgba(0, 0, 0, 0.2)' }}>
                     <div className='flex justify-center items-center w-full'>
