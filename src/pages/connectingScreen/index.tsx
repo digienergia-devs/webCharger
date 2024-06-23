0e8a1dc9d023c894eb089ffd8ebe32fb6a646afb
dimansha <dimansha.wijebandara@gmail.com>
2024-06-23 18:12:32 +0300

    connecting screen animation

--- src/pages/connectingScreen/index.tsx
+++ src/pages/connectingScreen/index.tsx
@@ -128,34 +128,28 @@ export default function ConnectingScreen(props: any) {
       <div className="flex justify-center items-center h-1/6">
         <img src={require("../../assets/icons/Final3.png")} alt="" />
       </div>
-      {/* <div className="flex flex-col justify-center items-center h-1/6 w-full">
-        <div className="flex justify-center items-center h-1/2"></div>
-        <div className="flex h-1/2 justify-center items-center text-center rounded-tl-30 rounded-tr-30 bg-iparkOrange400 w-5/6 shadow-md text-white font-bold text-xs md:text-md xl:text-xl">
-          <p className={headerInfo == 'Charger in use' ? "m-0 text-red-500" : "m-0"}>{headerInfo}</p>
-        </div>
-      </div> */}
       <div className="flex flex-col justify-start rounded-tl-30 rounded-tr-30 items-center h-5/6 w-screen bg-white pt-5">
-        <FadeLoader
+        {/* <FadeLoader
           color="#FF6D00"
           loading={true}
           aria-label="Loading Spinner"
           data-testid="loader"
-        />
+        /> */}
         <div
-          className="flex p-5 m-5 justify-center flex-col items-center rounded-tl-30 rounded-tr-30 rounded-bl-30 rounded-br-30 bg-gray-100 w-5/6 shadow-md text-gray-400 text-xs md:text-md xl:text-xl"
+          className="flex p-5 m-5 justify-center flex-col items-center rounded-tl-30 rounded-tr-30 rounded-bl-30 rounded-br-30 bg-gray-100 w-5/6 shadow-md text-black text-sm md:text-ms xl:text-xl"
           
         >
           <span>1.Connect cable to the car</span>
           <span>2.Connect cable to the socket</span>
         </div>
         <div
-          className="flex p-5 m-5 justify-center flex-col items-center w-5/6"
+          className="flex p-5 m-5 justify-center flex-col items-center w-5/6 -my-10"
           style={{ textShadow: "1px 1px 2px rgba(0, 0, 0, 0.2)" }}
         >
-          <Lottie animationData={connectCable} />
+          <Lottie className="flex h-1/2" animationData={connectCable} />
         </div>
         <div
-          className="flex p-5 m-5 justify-center flex-col items-center rounded-tl-30 rounded-tr-30 rounded-bl-30 text-center rounded-br-30 bg-gray-100 w-5/6 shadow-md text-gray-400 text-xs md:text-md xl:text-xl">
+          className="flex p-5 m-5 justify-center flex-col items-center rounded-tl-30 rounded-tr-30 rounded-bl-30 text-center rounded-br-30 bg-gray-100 w-5/6 shadow-md text-black text-sm md:text-md xl:text-xl">
 
           <span>
             We will process automatically once charger cable is connected
